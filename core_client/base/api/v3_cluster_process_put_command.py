9377c0efb24e83554c1b28219d9e2de0d61f40f8
Jan Stabenow <moin@jan-stabenow.de>
2024-12-08 23:56:17 +0100

    Add gpu limits

---
+++ core_client/base/api/v3_cluster_process_put_command.py
@@ -0,0 +1,54 @@
+import httpx
+from pydantic import TypeAdapter, validate_call
+
+from ...models import Client
+from ..models import Error
+from ..models.v3 import ProcessCommandAction
+
+
+@validate_call()
+def _build_request(
+    client: Client,
+    id: str,
+    command: ProcessCommandAction,
+    domain: str = "",
+    retries: int = None,
+    timeout: float = None,
+):
+    if not retries:
+        retries = client.retries
+    if not timeout:
+        timeout = client.timeout
+    return {
+        "method": "put",
+        "url": f"{client.base_url}/api/v3/cluster/process/{id}/command?domain={domain}",
+        "headers": client.headers,
+        "timeout": timeout,
+        "data": None,
+        "json": {"command": command},
+    }, retries
+
+
+def _build_response(response: httpx.Response):
+    if response.status_code == 200:
+        response_200 = response.json()
+        return response_200
+    else:
+        response_error = TypeAdapter(Error).validate_python(response.json())
+        return response_error
+
+
+def sync(client: Client, **kwargs):
+    request, retries = _build_request(client, **kwargs)
+    transport = httpx.HTTPTransport(retries=retries)
+    httpx_client = httpx.Client(transport=transport, http2=True)
+    response = httpx_client.request(**request)
+    return _build_response(response=response)
+
+
+async def asyncio(client: Client, **kwargs):
+    request, retries = _build_request(client, **kwargs)
+    transport = httpx.AsyncHTTPTransport(retries=retries)
+    async with httpx.AsyncClient(transport=transport) as httpx_client:
+        response = await httpx_client.request(**request)
+    return _build_response(response=response)
--- core_client/base/models/v3/process_config_limit.py
+++ core_client/base/models/v3/process_config_limit.py
@@ -9,8 +9,19 @@ class ProcessConfigLimit(BaseModel):
         "memory_mbytes": 0,
         "waitfor_seconds": 0
     }
+    
+    + {
+        "gpu_usage": 100,
+        "gpu_encoder": 100,
+        "gpu_decoder": 100,
+        "gpu_memory_mbytes": 2048
+    }
     """
 
     cpu_usage: Optional[float] = 0
     memory_mbytes: Optional[int] = 0
+    gpu_usage: Optional[float] = 0
+    gpu_encoder: Optional[float] = 0
+    gpu_decoder: Optional[float] = 0
+    gpu_memory_mbytes: Optional[int] = 0
     waitfor_seconds: Optional[int] = 0
